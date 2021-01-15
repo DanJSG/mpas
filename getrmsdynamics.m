f616935b3304f12f69eedd3a3c399e8852f2f133
DanJSG <jackson.daniel365@gmail.com>
2021-01-15 11:33:00 +0000

    Commented getrmsdynamics.m

--- getrmsdynamics.m
+++ getrmsdynamics.m
@@ -1,15 +1,34 @@
+%GETRMSDYNAMICS Function to calculate the RMS value of the amplitude of a
+%               signal over time.
+% Calculates the RMS amplitude over the length of the signal, calculating
+% it for segments determined by the given interval time.
+% Input arguments:
+%   audio - the audio signal
+%   Fs - the sampling frequency
+%   interval - the interval over which to calculate the RMS in seconds
 function[rmsDynamics] = getrmsdynamics(audio, Fs, interval)
     
-%     frameLength = 500e-3;
+    % Determine the frame length in samples based on the interval and
+    % sampling frequency
     frameLength = floor(interval * Fs);
+    
+    % Calculate the number of frames in the audio file
     nFrames = floor(length(audio) / frameLength) - 1;
 
+    % Initialise variable for the RMS values
     rmsDynamics = zeros(nFrames, 1);
+    
+    % Loop over each frame
     for n=1:nFrames
-
+        
+        % Calculate the start and finish samples
         startSample = ((n - 1) * frameLength) + 1;
         endSample = startSample + frameLength;
+        
+        % Extract the audio frame being analysed
         frame = audio(startSample:endSample);
+        
+        % Calculate the RMS value for the current frame of audio
         rmsDynamics(n) = rms(frame);
 
     end
