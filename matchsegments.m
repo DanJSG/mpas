89f7416dd5fbff4008602f061c7f358f5b316d99
DanJSG <jackson.daniel365@gmail.com>
2021-01-15 10:39:25 +0000

    Improved and commented matchsegments.m

--- matchsegments.m
+++ matchsegments.m
@@ -1,27 +1,33 @@
+%MATCHEDSEGMENTS Function to fit some data to a set number of samples
+%                points which last a certain duration.
+% Stretches or shrinks data with a set duration to have a different number
+% of sample points.
 function[matchedValues] = matchsegments(originalData, matchLength, duration)
     
+    % Setup the time domain
     matchedTimeDomain = linspace(0, duration, length(originalData));
+    
+    % Determine the length of each sample in time
     segmentLength = matchedTimeDomain(end) / matchLength;
-
+    
+    % Initialise value for storing output
     matchedValues = zeros(matchLength, 1);
-%     disp("Match length:" + matchLength);
-%     disp(matchedTimeDomain);
+    
+    % Loop over each sample in output
     for n=1:matchLength
         
-%         disp("Start: " + (n - 1) * segmentLength)
-%         disp("End: " + n * segmentLength);
+        % Get the original data's value that falls within the time segment
+        value = originalData(matchedTimeDomain <= n * segmentLength & matchedTimeDomain >= (n - 1) * segmentLength);
         
-        midiRange = matchedTimeDomain <= n * segmentLength & matchedTimeDomain >= (n - 1) * segmentLength;
-        indices = find(midiRange);
-        disp(indices);
-        value = originalData(indices);
+        % If multiple values are found, calculate the mean
         if length(value) > 1
             value = mean(value);
+        % If no values are found, then use the previous values
         elseif length(value) < 1 && n > 1
             value = matchedValues(n - 1);
         end
-%         disp("Value = " + value + " on iteration " + n + ".");
-%         disp("Value length = " + length(value) + ".");
+
+        % Update the current sample for the output
         matchedValues(n) = value;
     end
     
